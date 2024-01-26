0d13a76a5188dbce537ff4219486ed06019548b0
ElsaGregoire <elsa.gregoire@gmail.com>
2024-01-26 15:40:21 +0100

    updated the dim_products table in the WH to have our suppliers

--- ae_bootcamp/models/warehouse/dim_product.sql
+++ ae_bootcamp/models/warehouse/dim_product.sql
@@ -1,21 +1,23 @@
 WITH staging_source AS (
   SELECT
-      product_id
-    , product_code
-    , product_name
-    , description
-    , supplier_company
-    , standard_cost
-    , list_price
-    , reorder_level
-    , target_level
-    , quantity_per_unit
-    , discontinued
-    , minimum_reorder_quantity
-    , category
-    , attachments
-    , current_timestamp() as ingestion_timestamp
-    , FROM {{ ref('stg_products') }}
+      p.id as product_id
+    , p.product_code
+    , p.product_name
+    , p.description
+    , s.company as supplier_company
+    , p.standard_cost
+    , p.list_price
+    , p.reorder_level
+    , p.target_level
+    , p.quantity_per_unit
+    , p.discontinued
+    , p.minimum_reorder_quantity
+    , p.category
+    , p.attachments
+    , current_timestamp() AS ingestion_timestamp
+  FROM {{ ref('stg_products') }} p
+  LEFT JOIN {{ ref('stg_suppliers') }} s
+  ON s.id = p.supplier_id
 ),
 unique_source AS (
     SELECT
